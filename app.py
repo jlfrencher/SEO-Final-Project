22b1e5be3fb108b83ff84815116b1960cc4c7b79
Jacob Frencher <jlfrencher@gmail.com>
2022-07-28 22:38:21 +0000

    added User db.Model

--- app.py
+++ app.py
@@ -32,7 +32,24 @@ product_name = "Hybrid Schedule"
 #             'expiration_date': self.expiration_date.strftime("%m/%d/%Y"),
 #         }
 
-# db.create_all()
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    groupID = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'name': self.name,
+            'groupID': self.groupID,
+            'email': self.email,
+            'password': password
+        }
+
+db.create_all()
 
 @app.route("/")
 @app.route("/home")
